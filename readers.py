03bc241ab75b05799e8e0de1809841d50db4fb18
cjer <dbareket@gmail.com>
2019-08-07 17:44:35 +0300

    initial code commit

---
+++ readers.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from conllu import parse
+from collections import OrderedDict
+
+def get_conllu_df(path, remove_duplicates=False, remove_very_similar=False):
+    with open(path, 'r', encoding='utf8') as f:
+        sp_conllu = parse(f.read())
+    fixed = []
+    dup_to_remove = set()
+    very_sim_to_remove = set()
+    for tl in sp_conllu:
+        if (remove_duplicates and int(tl.metadata['sent_id']) in dup_to_remove 
+            or remove_very_similar and int(tl.metadata['sent_id']) in very_sim_to_remove):
+            print ('skipped', tl.metadata['sent_id'])
+            continue
+        for tok in tl:
+            t = OrderedDict(tok)
+            if type(t['id']) is not tuple:
+                if t['feats'] is not None:
+                    t.update({'feats_'+f: v for f, v in t['feats'].items()})
+                del(t['feats'])
+                if t['misc'] is not None:
+                    t.update({'misc_'+f: v for f, v in t['misc'].items()})
+                del(t['misc'])
+                t.update(tl.metadata)
+                fixed.append(t)
+            if remove_duplicates:
+                dup_to_remove = dup_to_remove | set(eval(tl.metadata['duplicate_sent_id']))
+            if remove_very_similar:
+                very_sim_to_remove = dup_to_remove | set(eval(tl.metadata['very_similar_sent_id']))
+
+    df = (pd.DataFrame(fixed)
+          .assign(sent_id = lambda x: x.sent_id.astype(int))
+          .assign(global_sent_id = lambda x: x.global_sent_id.astype(int))
+          .assign(misc_token_id = lambda x: x.misc_token_id.astype(int))
+
+         )
+    return df
+
+
+def make_conll_df(path, add_head_stuff=False):
+    # CoNLL file is tab delimeted with no quoting
+    # quoting=3 is csv.QUOTE_NONE
+    df = (pd.read_csv(path, sep='\t', header=None, quoting=3, comment='#',
+                names = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc'])
+                # add sentence labels
+                .assign(sent = lambda x: (x.id==1).cumsum())
+                # replace bad root dependency tags
+                .replace({'DEPREL': {'prd': 'ROOT'}})
+               )
+    
+    if add_head_stuff:
+        df = df.merge(df[['ID', 'FORM', 'sent', 'UPOS']].rename(index=str, columns={'FORM': 'head_form', 'UPOS': 'head_upos'}).set_index(['sent', 'ID']),
+               left_on=['sent', 'HEAD'], right_index=True, how='left')
+    return df
+
+
+def read_lattices(path):
+    df = (pd.read_csv(path, sep='\t', header=None, quoting=3, 
+                names = ['ID1', 'ID2', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'misc_token_id'])
+                # add sentence labels
+                .assign(sent = lambda x: (x.ID1==0).cumsum())
+               )
+    return df
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+
+def get_feats(s):
+    if s!='_' and s is not None and s is not np.nan:
+        feats = OrderedDict()
+        for f in s.split('|'):
+            k,v = f.split('=')
+            k='feats_'+k
+            if k not in feats:
+                feats[k] = v
+            else:
+                feats[k] = feats[k]+','+v
+        return pd.Series(feats)
+    else:
+        return pd.Series()
+
+    
+def get_yap_output_df(tokens_path, dep_path, map_path):
+    tokens = dict(flatten([[(str(j+1)+'_'+str(i+1), tok) for i, tok in enumerate(sent.split('\n'))]
+              for j, sent in 
+              enumerate(open(os.path.join(yap_output_dir, tokens_path), 'r').read().split('\n\n'))]))
+    lattices = read_lattices(map_path)
+    dep = make_conll_df(dep_path)
+    df = (pd.concat([dep, lattices.misc_token_id], axis=1)
+          .assign(sent_tok = lambda x: x.sent.astype(str) + '_' + x.misc_token_id.astype(str))
+          .assign(misc_token_str = lambda x: x.sent_tok.map(tokens))
+          .drop('sent_tok', axis=1)
+          )
+    df = pd.concat([df, df.feats.apply(get_feats)], axis=1).drop('feats', axis=1)
+    return df
---
+++ transformers.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+def get_token_biose(df, biose_field='misc_biose'):
+    
+    def _single_token_conversion(tok):
+        all_bio = tok.biose_only.tolist()
+        all_typ = set(tok.ner_type.dropna().tolist())
+        if len(all_typ)>1:
+            return 'O'
+        if 'S' in all_bio:
+            new_bio = 'S'
+        elif 'B' in all_bio and 'E' in all_bio:
+            new_bio = 'S'
+        elif 'B' in all_bio:
+            new_bio = 'B'
+        elif 'E' in all_bio:
+            new_bio = 'E'
+        elif 'I' in all_bio:
+            new_bio = 'I'
+        else:
+            return 'O'
+        return new_bio+'-'+all_typ.pop()
+    
+    df[['biose_only', 'ner_type']] = spdf[biose_field].str.split('-', expand=True)
+    df = (df
+          .groupby(['sent_id', 'misc_token_id', 'misc_token_str'])
+          .apply(_single_token_conversion)
+          .reset_index().rename(columns={0:'biose'})
+         )
+    return df
+
+def get_token_df(df, fields=None, sep='^', fill_value='', biose=True):
+    tok_dfs = []
+    
+    if biose:
+        tok_dfs.append(get_token_biose(df))
+        
+    if fields is not None:
+        concat_fields = lambda x: pd.Series({f: sep.join(x[f].fillna(fill_value).tolist()) for f in fields})
+        tok_fields = (df
+                .groupby(['sent_id', 'misc_token_id', 'misc_token_str'])
+                .apply(concat_field))
+        tok_dfs.append(tok_fields)
+        
+    return tok_df
